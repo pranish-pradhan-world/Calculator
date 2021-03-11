274cf7d4ca93ccb7129f63e2d8ee36e3e9cd6ff8
pranish-pradhan-world <80395995+pranish-pradhan-world@users.noreply.github.com>
2021-03-11 14:18:34 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function calc(){
+    let a = parseInt(document.querySelector("#value1").value);
+    let b = parseInt(document.querySelector("#value2").value);
+    let op = document.querySelector("#operator").value;
+    let calculate;
+
+    if(op == "add"){
+        calculate = a + b;
+    } else if (op == "sub"){
+        calculate = a - b;
+    } else if (op == "mul"){
+        calculate = a * b;
+    } else if (op == "div"){
+        calculate = a / b;
+    }
+
+    document.querySelector("#result").innerHTML = calculate;
+}
